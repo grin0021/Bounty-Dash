371ca8dfcb746695e9199cdfbd4d4dc7fee74c9a
grin0021 <55245260+grin0021@users.noreply.github.com>
2022-07-31 19:19:47 -0400

    Delete BountyDash directory

--- BountyDash/Source/BountyDash/BountyDash.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "BountyDash.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, BountyDash, "BountyDash" );
